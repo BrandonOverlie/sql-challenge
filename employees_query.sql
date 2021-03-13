89c7d9516b5394042269f1095250bf3f2acf3953
Brandon O <brandon.overlie@gmail.com>
2021-03-13 16:18:50 -0600

    code works! just need to make sure I'm meeting submittal reqs

--- employees_query.sql
+++ employees_query.sql
@@ -93,4 +93,25 @@ where first_name = 'Hercules'
 and last_name like 'B%';
 
 -- List the employee number, last name, first name and department name of all employees in the Sales department
-select 
+select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from dept_emp
+join employees
+on dept_emp.emp_no = employees.emp_no
+join departments
+on dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales';
+
+-- List the employee number, last name, first name adn department name of all employees in the Sales and Development departments
+select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from dept_emp
+join employees
+on dept_emp.emp_no = employees.emp_no
+join departments
+on dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales' or departments.dept_name = 'Development';
+
+-- List the frequency count of employee last names, in descending order
+select last_name, count(last_name) as "employee last names count"
+from employees
+group by last_name
+order by "employee last names count" desc;
